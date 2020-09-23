fd1fac51925a463efcf42e8c7fc5eecf3628f797
Kimberly K <kimberly.n.kubo@gmail.com>
2020-09-23 11:24:08 -0700

    Added a function to start the chronometer when the button is pressed

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_main_start.setOnClickListener {
+            chronometer_main_time.start()
+        }
     }
 }
