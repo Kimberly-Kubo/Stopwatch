b04ff42ea5d717b6703cf0e9fc14f297a2622c19
Kimberly K <kimberly.n.kubo@gmail.com>
2020-10-07 11:23:07 -0700

    Set the start button enabled to false after pressing start and true when pressing stop or reset to avoid the problem where pressing start again would reset the chronometer.

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -11,11 +11,45 @@ class MainActivity : AppCompatActivity() {
 
     private val TAG = "MainActivity"
     private var wasRunning = false
+    private var timeWhenStopped = 0L
+    //private val KEY_TIME_ELAPSED = "elapsed time"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate has been called")
         setContentView(R.layout.activity_main)
+
+        //?. builds an if not null
+        //?: is the elvis operator. what comes after is the default value is what comes before is null
+        //timeElapsed = savedInstanceState?.getLong(KEY_TIME_ELAPSED) ?: 0L
+
+        button_main_start.setOnClickListener {
+            if (!wasRunning)
+            {
+                chronometer_main_time.base = SystemClock.elapsedRealtime()
+            }
+            else
+            {
+                chronometer_main_time.base = SystemClock.elapsedRealtime() - timeWhenStopped
+            }
+            chronometer_main_time.start()
+            timeWhenStopped = 0
+            wasRunning = true
+            button_main_start.isEnabled = false
+        }
+        button_main_stop.setOnClickListener {
+            timeWhenStopped = SystemClock.elapsedRealtime() - chronometer_main_time.base
+            chronometer_main_time.stop()
+            wasRunning = true
+            button_main_start.isEnabled = true
+        }
+        button_main_reset.setOnClickListener {
+            chronometer_main_time.base = SystemClock.elapsedRealtime()
+            chronometer_main_time.stop()
+            wasRunning = false
+            timeWhenStopped = 0
+            button_main_start.isEnabled = true
+        }
     }
     override fun onStart() {
         super.onStart()
@@ -25,24 +59,6 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         Log.d(TAG, "onResume had been called")
-
-        button_main_start.setOnClickListener {
-            if (!wasRunning)
-            {
-                chronometer_main_time.base = SystemClock.elapsedRealtime()
-            }
-            chronometer_main_time.start()
-            wasRunning = true
-        }
-        button_main_stop.setOnClickListener {
-            chronometer_main_time.stop()
-            wasRunning = true
-        }
-        button_main_reset.setOnClickListener {
-            chronometer_main_time.base = SystemClock.elapsedRealtime()
-            chronometer_main_time.stop()
-            wasRunning = false
-        }
     }
     override fun onPause()
     {
